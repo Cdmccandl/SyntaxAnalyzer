2522246b0d1d1188608b95b01f73d7e3dcc3f09a
Casey Jones <cjone183@msudenver.edu>
2019-09-29 21:58:32 -0600

    remove spaces from filenames for consistency

---
+++ input/source20.pas
@@ -0,0 +1,19 @@
+program Source 20
+
+  var
+    x: integer;
+    y: integer;
+    result: boolean
+
+  begin
+
+    read x;;
+    read y;
+    if x > y then
+        result := true;
+    else    
+        result := false;
+    write result
+
+  end.
+
